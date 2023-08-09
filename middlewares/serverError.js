c83a05fddf1e35f148dce9b92e238c3d6963843a
Alexashka-2022 <avibus.bonis.2022@gmail.com>
2023-08-09 23:08:09 +0300

    Исправлено сообщение для статуса 500

--- middlewares/serverError.js
+++ middlewares/serverError.js
@@ -1,5 +1,5 @@
 module.exports.serverError = (err, req, res, next) => {
   const { status = 500, message } = err;
-  res.status(status).send({ message: status === 500 ? 'Сервер передал ошибку' : message });
+  res.status(status).send({ message: status === 500 ? ('Сервер передал ошибку', err.message) : message });
   next();
 };
